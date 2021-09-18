f27f386fffe82fcef04727a3ef93140575e3e33b
e11438 <isankalpa7@gmail.com>
2021-09-18 17:52:11 +0530

    enhanced static function cpp

--- static/staticDataAndFunctions.cpp
+++ static/staticDataAndFunctions.cpp
@@ -7,6 +7,11 @@ class Animal
     public:
     string name;
     static string type; // since this is common for all the objs explicit declaration is needed
+
+    static void printType() //static functions can access only static members
+    {
+        cout << type << endl;
+    }
 };
 
 string Animal::type; // should allocate memory for static data members: since this is common, no object creation will create memory for this 
@@ -21,5 +26,9 @@ int main()
     a.type = "Animal";
     cout << a.name << " - "<< a.type << endl;
     cout << b.name << " - "<< b.type << endl;
+
+    cout << "access static member "<< Animal::type << endl;
+    a.printType();  //calling static function from obj
+    Animal::printType(); // calling static function derectly
     return 0;
 }
